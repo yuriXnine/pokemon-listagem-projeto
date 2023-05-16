4ad12196623d2d6368fdb04138ff6945e26ef41d
yuriXnine <96672419+yuriXnine@users.noreply.github.com>
2023-05-16 11:27:45 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -11,9 +11,9 @@ botaoAlterarTema.addEventListener("click", () => {
     body.classList.toggle("modo-escuro")
 
     if (modoEscuroEstaAtivo) {
-    imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/sun.png");
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
     }   else {
-    imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/moon.png");
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
     }
 });
 
